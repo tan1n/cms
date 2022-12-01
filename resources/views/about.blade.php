a3511c974c080acf27802d16ebda5c675de6694b
Tanin Rahman <taninrahman07@gmail.com>
2022-12-01 22:42:38 +0600

    link removal from about us

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -80,21 +80,21 @@
 
                     <div class="icon-box wow fadeInUp">
                         <div class="icon"><i class="fa fa-shopping-bag"></i></div>
-                        <h4 class="title"><a href="#">{{ setting('about-us.headline_1') }}</a></h4>
+                        <h4 class="title"><a href="javascript:void(0);">{{ setting('about-us.headline_1') }}</a></h4>
                         <p class="description">{{ setting('about-us.sub_1') }}</p>
                     </div>
 
                     <div class="icon-box wow fadeInUp"
                          data-wow-delay="0.2s">
                         <div class="icon"><i class="fa fa-photo"></i></div>
-                        <h4 class="title"><a href="#">{{ setting('about-us.headline_2') }}</a></h4>
+                        <h4 class="title"><a href="javascript:void(0);">{{ setting('about-us.headline_2') }}</a></h4>
                         <p class="description">{{ setting('about-us.sub_2') }}</p>
                     </div>
 
                     <div class="icon-box wow fadeInUp"
                          data-wow-delay="0.4s">
                         <div class="icon"><i class="fa fa-bar-chart"></i></div>
-                        <h4 class="title"><a href="#">{{setting('about-us.headline_3')}}</a></h4>
+                        <h4 class="title"><a href="javascript:void(0);">{{setting('about-us.headline_3')}}</a></h4>
                         <p class="description">{{ setting('about-us.sub_3') }}</p>
                     </div>
                 </div>
